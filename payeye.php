8c3be102e729d25cc3f1132d65c0c7b452603648
Bartosz Bury <bartosz.bury@payeye.com>
2023-03-22 14:04:52 +0000

    [Gl 6482] remove mock

--- payeye.php
+++ payeye.php
@@ -41,7 +41,7 @@ class PayEye extends PaymentModule
     {
         $this->name = 'payeye';
         $this->tab = 'payments_gateways';
-        $this->version = '0.0.3';
+        $this->version = '0.0.5';
         $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
         $this->author = 'PayEye';
         $this->controllers = ['Cart', 'Order', 'OrderUpdate', 'Widget'];
