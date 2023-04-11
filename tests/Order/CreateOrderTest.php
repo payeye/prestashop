e210fd31fa46f6a97b7aa33527be0b5f37d25024
Bartosz Bury <bartosz.bury@payeye.com>
2023-04-11 07:21:27 +0000

    PoC Paczkomaty

--- tests/Order/CreateOrderTest.php
+++ tests/Order/CreateOrderTest.php
@@ -3,6 +3,9 @@
 namespace PayEye\Tests\Order;
 
 use PayEye\Lib\Cart\CartResponseModel;
+use PayEye\Lib\Enum\PickupPointType;
+use PayEye\Lib\Model\Location;
+use PayEye\Lib\Model\PickupPoint;
 use PayEye\Lib\Order\OrderResponseModel;
 use PayEye\Tests\Shared\BaseTestCase;
 
@@ -31,5 +34,24 @@ class CreateOrderTest extends BaseTestCase
         $this->assertSame(strlen($response->currency), $isoCurrency);
         $this->assertIsString($response->checkoutUrl);
         $this->assertSame($this->module->orderStatuses->getCreated(), (int) $order->current_state, 'Order status after created order is not correct');
+    }
+
+    public function testCreateOrderWithPaczkomat(): void
+    {
+        $pickupPoint = PickupPoint::builder()
+            ->setName('WRO160M,Zielińskiego 61,53-533 Wrocław')
+            ->setLocation(Location::builder()->setLat(55.234234)->setLng(54.234234))
+            ->setType(PickupPointType::PARCEL_LOCKER);
+
+        $this->getCart($this->mock);
+        $cart = CartResponseModel::createFromArray($this->response->getArrayResponse());
+
+        $mock = $this->mock;
+        $mock['shippingId'] = $cart->shippingId;
+        $mock['cartHash'] = $cart->cartHash;
+        $mock['shipping']['pickupPoint'] = $pickupPoint->toArray();
+        $this->createOrder($mock);
+
+        $response = OrderResponseModel::createFromArray($this->response->getArrayResponse());
     }
 }
