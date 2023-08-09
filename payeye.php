0433a2d80016032d2818efec25dabc66fa76f611
bartosz.bury <bartosz.bury@payeye.com>
2023-08-09 08:54:14 +0200

    up version

--- payeye.php
+++ payeye.php
@@ -47,7 +47,7 @@ class PayEye extends PaymentModule
     {
         $this->name = 'payeye';
         $this->tab = 'payments_gateways';
-        $this->version = '0.0.29';
+        $this->version = '0.0.30';
         $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
         $this->author = 'PayEye';
         $this->controllers = ['Cart', 'Order', 'OrderUpdate', 'Widget', 'Return'];
