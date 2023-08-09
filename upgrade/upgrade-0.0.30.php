e90545586ee29f70f2b05e300f8e1e96f98ff63a
bartosz.bury <bartosz.bury@payeye.com>
2023-08-09 09:07:03 +0200

    add upgrade

---
+++ upgrade/upgrade-0.0.30.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PayEye
+ *
+ * @author    PayEye
+ * @copyright Copyright (c) 2023 PayEye
+ * @license   http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
+ *
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_0_0_30($module)
+{
+    $module->registerHook('moduleRoutes');
+    $module->registerHook('actionFrontControllerSetMedia');
+
+    return true;
+}
