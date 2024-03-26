baf5386b97880ec99ba7b99c2bac219b1305c4e3
Mark Kantsevich <mark.kantsevich@payeye.com>
2024-03-26 16:17:58 +0100

    Added endpoint POST plugin/status for changing the status on the backoffice side.

--- controllers/front/PluginStatus.php
+++ controllers/front/PluginStatus.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use PayEye\Lib\Enum\PluginEvents;
+use PayEye\Lib\Enum\PluginModes;
+use PayEye\Lib\Plugin\PluginStatusRequestModel;
+use PayEye\Lib\Exception\PayEyePaymentException;
 use PrestaShop\Module\PayEye\Controller\FrontController;
 use PrestaShop\Module\PayEye\Admin\Configuration\ConfigurationField;
 use PayEye\Lib\Enum\SignatureFrom;
@@ -15,13 +19,46 @@ class PayEyePluginStatusModuleFrontController extends FrontController
 
     public function postProcess()
     {
-        $this->checkPermission([
-            'signatureFrom' => SignatureFrom::PLUGIN_STATUS_REQUEST,
-            'shopIdentifier' => $this->module->authConfig->getShopId(),
-            'signature' => Tools::getValue('signature')
-        ]);
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $this->checkPermission([
+                        'signatureFrom' => SignatureFrom::PLUGIN_STATUS_REQUEST,
+                        'shopIdentifier' => $this->module->authConfig->getShopId(),
+                        'signature' => (string)Tools::getValue('signature')
+                    ]);
+                    $this->exitWithResponse($this->pluginStatus(PluginEvents::PLUGIN_INFO));
+                    break;
+                case 'POST':
+                    $request = $this->getRequest();
+                    $this->checkPermission($request);
+                    $request = PluginStatusRequestModel::createFromArray($request);
+                    $this->changeConfig($request);
+                    $this->exitWithResponse($this->pluginStatus(PluginEvents::PLUGIN_CONFIG_CHANGED));
+            }
+        } catch (PayEyePaymentException $exception) {
+            $this->exitWithPayEyeExceptionResponse($exception);
+        } catch (Exception|Throwable $exception) {
+            $this->exitWithExceptionMessage($exception);
+        }
+    }
 
-        $pluginMode = Configuration::get(ConfigurationField::TEST_MODE) ? 'INTEGRATION' : 'PRODUCTION';
+    protected function changeConfig(PluginStatusRequestModel $request)
+    {
+        $testMode = 0;
+        if ($request->getPluginMode() === PluginModes::PLUGIN_MODE_INTEGRATION) {
+            $testMode = 1;
+        }
+        Configuration::updateValue(ConfigurationField::TEST_MODE, $testMode);
+    }
+
+    /**
+     * @param $pluginEvent
+     * @return array
+     */
+    protected function pluginStatus($pluginEvent)
+    {
+        $pluginMode = Configuration::get(ConfigurationField::TEST_MODE) ? PluginModes::PLUGIN_MODE_INTEGRATION : PluginModes::PLUGIN_MODE_PRODUCTION;
 
         $response = [
             'apiVersion' => $this->module->getApiVersion(),
@@ -30,11 +67,11 @@ class PayEyePluginStatusModuleFrontController extends FrontController
             'languageVersion' => 'PHP ' . phpversion(),
             'platformVersion' => _PS_VERSION_,
             'pluginVersion' => $this->module->version,
-            'pluginEvent' => 'PLUGIN_ACTIVATED',
+            'pluginEvent' => $pluginEvent,
             'pluginConfig' => null,
             'signatureFrom' => SignatureFrom::PLUGIN_UPDATE_STATUS_REQUEST
         ];
 
-        $this->exitWithResponse($response);
+        return $response;
     }
 }
